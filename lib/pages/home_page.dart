f8cee666f925c11a08499e849811365ab5cb4a40
Maksym Khamuliak <maksymkhamuliak@gmail.com>
2025-04-28 11:15:03 +0300

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/utils/emoji_translator.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  String _translatedText = '';
+
+  void _onTextChanged(String text) {
+    setState(() {
+      _translatedText = EmojiTranslator.translate(text);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Emoji Translator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          spacing: 20,
+          children: [
+            TextField(
+              controller: _controller,
+              onChanged: _onTextChanged,
+              decoration: const InputDecoration(
+                labelText: 'Write something interesting',
+                border: OutlineInputBorder(),
+              ),
+              maxLines: null,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  _translatedText.isEmpty 
+                  ? 'Start typing to see emoji translation!' 
+                  : _translatedText,
+                  style: const TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
